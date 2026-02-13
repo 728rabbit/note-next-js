dcebc362195a21099a008fe2b4f1e3f5c91bed2a
Kaiyun <kaiyunchan2012@hotmail.com>
2026-02-13 14:08:05 +0800

    Add authentication proxy middleware
    Implement middleware for handling authentication and redirects based on token presence.

---
+++ proxy.ts
@@ -0,0 +1,48 @@
+// middleware - root dir, there can only be one project. 
+
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function proxy(request: NextRequest) {
+    const { pathname } = request.nextUrl;
+    const token = request.cookies.get('token');
+
+    console.log('Middleware 執行:', pathname);
+
+    if(pathname === '/admin/authn/login') {
+        if (token) {
+            return NextResponse.redirect(new URL('/', request.url));
+        }
+        return NextResponse.next();
+    }
+
+    // Skip middleware for static files and API routes
+    if (
+        pathname.startsWith('/_next') ||  // Next.js internal files
+        pathname.startsWith('/static') ||  // Static files
+        pathname.startsWith('/favicon.ico') || // Favicon
+        pathname.startsWith('/api')  // API routes (if you have them)
+    ) {
+        return NextResponse.next();
+    }
+
+    if (!token && pathname.startsWith('/admin')) {
+        // 沒有 token，重定向到登入頁
+        return NextResponse.redirect(new URL('/admin/authn/login', request.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public folder files
+     */
+    '/((?!_next/static|_next/image|favicon.ico).*)',
+  ],
+};
